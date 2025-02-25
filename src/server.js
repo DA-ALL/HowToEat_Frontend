00a03c738b78f5f765ddcf1c5653ed5ff9599992
Yeahyun Kim <insidesy4@gmail.com>
2025-02-25 16:31:45 +0900

    [Add] src 폴더 추가 및 server.js 경로 변경

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public/user')); // public 폴더 내 정적 파일 제공
+app.use(express.static('../public/user')); // public 폴더 내 정적 파일 제공
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/user', 'main.html')); // 기본 파일 변경
+    res.sendFile(path.join(__dirname, '../public/user', 'main.html')); // 기본 파일 변경
 });
 
 app.listen(port, () => {
@@ -15,5 +15,5 @@ app.listen(port, () => {
 });
 
 app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/admin', 'admin.html'));
+    res.sendFile(path.join(__dirname, '../public/admin', 'admin.html'));
 });
