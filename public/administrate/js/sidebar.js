c6962103a2c786eadfd7a10d0f9cd66e94ac51b3
Yeahyun Kim <insidesy4@gmail.com>
2025-03-10 14:48:33 +0900

    [Add] sidebar 아이콘 추가 #1

--- public/administrate/js/sidebar.js
+++ public/administrate/js/sidebar.js
@@ -9,35 +9,36 @@ $(document).ready(function () {
             <div class="sidebar-item-wrapper nav-link" data-page="dashboard">
                 <div class="point-rect"></div>
                 <div class="sidebar-item"> 
-                    <img class="logo-dashboard" src="/administrate/images/icon_dashboard.png">
+                    <div class="icon-sidebar">
+                        <img class="logo-dashboard" src="/administrate/images/icon_dashboard.png">
+                    </div>
                     <div class="label">대시보드</div>
                 </div>
             </div>
             <div class="sidebar-item-wrapper nav-link" data-page="user-management">
                 <div class="point-rect"></div>
-                <div class="sidebar-item"> 
-                    <img class="logo-user" src="/administrate/images/icon_user.png">
+                <div class="sidebar-item">
+                    <div class="icon-sidebar">
+                        <img class="logo-user" src="/administrate/images/icon_user.png">
+                    </div>
                     <div class="label">유저관리</div>
                 </div>
             </div>
             <div class="sidebar-item-wrapper nav-link" data-page="food-management">
                 <div class="point-rect"></div>
                 <div class="sidebar-item"> 
-                    <img class="logo-food" src="/administrate/images/icon_food.png">
+                    <div class="icon-sidebar">
+                        <img class="logo-food" src="/administrate/images/icon_food.png">
+                    </div>
                     <div class="label">음식관리</div>
-                </div>
-            </div>
-            <div class="sidebar-item-wrapper nav-link" data-page="report">
-                <div class="point-rect"></div>
-                <div class="sidebar-item"> 
-                    <img class="logo-report" src="/administrate/images/icon_report.png">
-                    <div class="label">리포트</div>
                 </div>
             </div>
             <div class="sidebar-item-wrapper nav-link" data-page="notice">
                 <div class="point-rect"></div>
-                <div class="sidebar-item"> 
-                    <img class="logo-notice" src="/administrate/images/icon_notice.png">
+                <div class="sidebar-item">
+                    <div class="icon-sidebar">
+                        <img class="logo-notice" src="/administrate/images/icon_notice.png">
+                    </div>
                     <div class="label">공지사항</div>
                 </div>
             </div>
@@ -47,7 +48,9 @@ $(document).ready(function () {
             <div class="sidebar-item-wrapper nav-link" data-page="admin-management">
                 <div class="point-rect"></div>
                 <div class="sidebar-item"> 
-                    <img class="logo-admin" src="/administrate/images/icon_admin.png">
+                    <div class="icon-sidebar">
+                        <img class="logo-admin" src="/administrate/images/icon_admin.png">
+                    </div>
                     <div class="label">어드민 관리</div>
                 </div>
             </div>                
@@ -104,5 +107,25 @@ $(document).ready(function () {
     function updateActiveState(page) {
         $(".nav-link").removeClass("active"); // 기존 active 제거
         $(`.nav-link[data-page="${page}"]`).addClass("active"); // 새로 선택된 요소에 active 추가
+
+        // 모든 nav-link 아이콘을 기본 이미지로 설정
+        $('.logo-dashboard').attr('src', '/administrate/images/icon_dashboard.png');
+        $('.logo-user').attr('src', '/administrate/images/icon_user.png');
+        $('.logo-food').attr('src', '/administrate/images/icon_food.png');
+        $('.logo-notice').attr('src', '/administrate/images/icon_notice.png');
+        $('.logo-admin').attr('src', '/administrate/images/icon_admin.png');
+
+        // 현재 활성화된 페이지의 아이콘을 하얀색 이미지로 변경
+        if (page === 'dashboard') {
+            $('.logo-dashboard').attr('src', '/administrate/images/icon_dashboard_white.png');
+        } else if (page === 'user-management') {
+            $('.logo-user').attr('src', '/administrate/images/icon_user_white.png');
+        } else if (page === 'food-management') {
+            $('.logo-food').attr('src', '/administrate/images/icon_food_white.png');
+        } else if (page === 'notice') {
+            $('.logo-notice').attr('src', '/administrate/images/icon_notice_white.png');
+        } else if (page === 'admin-management') {
+            $('.logo-admin').attr('src', '/administrate/images/icon_admin_white.png');
+        }
     }
 });
