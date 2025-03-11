38434584abe972a591e8b74f1499cc6d537fe930
Yeahyun Kim <insidesy4@gmail.com>
2025-03-11 19:04:17 +0900

    [Fix] 이미지 명칭 변경

--- public/administrate/js/profilebar.js
+++ public/administrate/js/profilebar.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
                 <div class="label-name">김명현</div>
                 <div class="label-role">Master</div>
             </div>
-            <img src="/administrate/images/button_profile.png" class="button-profile">
+            <img src="/administrate/images/icon_toggle_arrow_down.png" class="button-profile">
         </div>
     `);
     
