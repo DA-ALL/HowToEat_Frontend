be5e1c45faf36f2785955633e3798dd026920958
Yeahyun Kim <insidesy4@gmail.com>
2025-03-13 15:06:30 +0900

    [Add] 로고 이미지 추가 #18

--- public/user/js/loginPage.js
+++ public/user/js/loginPage.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     $('#loginPage').html(`
         <div class="login-container">
+            <div class="logo">
+                <img src="/user/images/logo_01.png">
+            </div>
         </div>
     `);
     
