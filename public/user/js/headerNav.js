5793ca6c63e93856b78bb84e84646777df32a295
Yeahyun Kim <insidesy4@gmail.com>
2025-03-14 18:07:45 +0900

    [Feat] header-nav 컴포넌트 화 작업완료 #24

---
+++ public/user/js/headerNav.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    //  [type에 따른 header 구성]
+    //  - type 1 =  뒤로가기 O / 타이틀 X  / 추가하기버튼 X
+    //  - type 2 =  뒤로가기 O / 타이틀 O / 추가하기버튼 X
+    //  - type 3 =  뒤로가기 O / 타이틀 O / 추가하기버튼 O
+
+    let $headerNav = $('#headerNav');
+    let title = $headerNav.data('title');
+    let type = $headerNav.data('type');
+
+    let headerTemplate = '';
+
+    switch (type) {
+        case 1:
+            headerTemplate = `
+                <div class="header-nav">
+                    <div class="button-prev">
+                        <img src="/user/images/icon_arrow_back.png">
+                    </div>
+
+                    <div class="title hidden">${title}</div>
+
+                    <div class="button-add hidden">
+                        <img src="/user/images/icon_add.png">
+                    </div>
+                </div>
+            `;
+            break;
+
+        case 2:
+            headerTemplate = `
+                <div class="header-nav">
+                    <div class="button-prev">
+                        <img src="/user/images/icon_arrow_back.png">
+                    </div>
+
+                    <div class="title">${title}</div>
+
+
+                    <div class="button-add hidden">
+                        <img src="/user/images/icon_add.png">
+                    </div>
+                </div>
+            `;
+            break;
+
+        default:
+            headerTemplate = `
+                <div class="header-nav">
+                    <div class="button-prev">
+                        <img src="/user/images/icon_arrow_back.png">
+                    </div>
+
+                    <div class="title">${title}</div>
+
+                    <div class="button-add">
+                        <img src="/user/images/icon_add.png">
+                    </div>
+                </div>
+            `;
+    }
+
+    $headerNav.html(headerTemplate);
+});
