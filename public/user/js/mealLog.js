1bb0818f299b7052757edfde3ef7e4f125e0ea9d
Yeahyun Kim <insidesy4@gmail.com>
2025-03-27 21:02:03 +0900

    [Feat] 식사기록 스타일 작업 진행 #34

---
+++ public/user/js/mealLog.js
@@ -0,0 +1,50 @@
+export function getMealLog() {
+    return `
+        <div class="meal-log-title">식사기록</div>
+        <div class="log-container">
+            <div class="log-wrapper">
+                <div class="meal-time">아침</div>
+                <div class="meal-kcal-wrapper">
+                    <div class="consumed-kcal">542</div>
+                    <div class="divide">/</div>
+                    <div class="target-kcal">612kcal</div>
+                    <div class="icon-arrow">
+                        <img src="/user/images/icon_arrow_front.png">
+                    </div>
+                </div>
+            </div>
+            <div class="log-wrapper">
+                <div class="meal-time">점심</div>
+                <div class="meal-kcal-wrapper">
+                    <div class="consumed-kcal">542</div>
+                    <div class="divide">/</div>
+                    <div class="target-kcal">612kcal</div>
+                    <div class="icon-arrow">
+                        <img src="/user/images/icon_arrow_front.png">
+                    </div>
+                </div>
+            </div>
+            <div class="log-wrapper">
+                <div class="meal-time">저녁</div>
+                <div class="meal-kcal-wrapper">
+                    <div class="consumed-kcal">542</div>
+                    <div class="divide">/</div>
+                    <div class="target-kcal">612kcal</div>
+                    <div class="icon-arrow">
+                        <img src="/user/images/icon_arrow_front.png">
+                    </div>
+                </div>
+            </div>
+            <div class="log-wrapper">
+                <div class="meal-time">간식</div>
+                <div class="meal-kcal-wrapper">
+                    <div class="consumed-kcal">542</div>
+                    <div class="icon-arrow">
+                        <img src="/user/images/icon_arrow_front.png">
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    `;
+}
