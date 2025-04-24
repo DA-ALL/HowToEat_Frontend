9852e007116f2ee7188e92e187f6a856c7303139
Yeahyun Kim <insidesy4@gmail.com>
2025-04-24 11:53:13 +0900

    [Feat] 공지사항 디테일 페이지 작업 #41

--- public/user/js/my-page/usersNoticeDetail.js
+++ public/user/js/my-page/usersNoticeDetail.js
@@ -1,26 +1,26 @@
 export function renderUsersNoticeDetail(id) {
     return `
-        <div class="set-container">
             <div id="headerNav" data-title="공지사항" data-type="2"></div>
-            <div class="item-container">
-                <div class="title">아침</div>
-                <div class="time breakfast">08:15</div>
+            <div class="notice-info-container">
+            <div class="notice-container">
+                <div class="notice-type">업데이트</div>
+                <div class="notice-title">하잇앱이 신규 업데이트 되었어요</div>
+                <div class="notice-date">2025.03.30</div>
             </div>
-            <div class="item-container">
-                <div class="title">점심</div>
-                <div class="time lunch">12:45</div>
+
+            <div class="notice-description">
+                안녕하세요.</br>
+                howToEat 하잇 입니다.</br>
+                </br>
+                이번에 하잇앱의 새로운 기능 OOO이 업데이트 되었습니다.</br>
+                많은 이용 부탁드리며, 불편하신 사항은 언제든지 문의 게시판에
+                남겨주시면 빠른 조치 드릴것을 약속드립니다.</br>
+                </br>
+                하잇을 이용해주셔서 감사드리며, 앞으로도 많은 이용 부탁드립니다. </br>
+                감사합니다.
+            
             </div>
-            <div class="item-container">
-                <div class="title">저녁</div>
-                <div class="time dinner">19:35</div>
             </div>
-            <div class="recommend-container">
-                <div class="text-wrapper">
-                    <div class="title">추천 시간으로 설정</div>
-                    <div class="sub-title">하잇에서 추천해 주는 식사 시간으로 설정돼요</div>
-                </div>
-                <div class="recommend-set-time-toggle"></div>
-            </div>
-        </div>
+    
     `;
 } 
