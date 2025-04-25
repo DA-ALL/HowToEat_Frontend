5cabc8bcf635686d3ca3194bb4679313d435f217
Yeahyun Kim <insidesy4@gmail.com>
2025-04-25 11:25:57 +0900

    [Feat] 나의 키와 몸무게 영역 작업 진행 #41

--- public/user/js/my-page/usersInfo.js
+++ public/user/js/my-page/usersInfo.js
@@ -1,4 +1,9 @@
 export function renderUsersInfo() {
+    let stats = "체중감량";
+
+    // 유틸 함수로 valid 클래스 조건 처리
+    const isValid = (label) => stats === label ? 'valid' : '';
+
     return `
         <div id="headerNav" data-title="나의 정보" data-type="2"></div>
         <div class="profile-container">
@@ -10,8 +15,18 @@ export function renderUsersInfo() {
             <div class="profile-name">김예현</div>
             <div class="profile-email">insidesy4@gmail.com</div>
         </div>
+
+        <div class="body-stats-container container-format">
+            <div class="info-title">나의 키와 몸무게</div>
+            <div class="select-container inner-container-format">
+                <div class="select-item weight-loss ${isValid("체중감량")}" data-text="1">체중 감량</div>
+                <div class="select-item weight-maintain ${isValid("체중유지")}" data-text="2">체중 유지</div>
+                <div class="select-item weight-gain ${isValid("체중증량")}" data-text="3">체중 증량</div>
+                <div class="select-item muscle-gain ${isValid("근육증량")}" data-text="4">근육 증량</div>
+            </div>
+        </div>
     `;
-} 
+}
 
 
 // 이미지 클릭 시 파일 선택창 열기
@@ -39,4 +54,10 @@ $(document).off('change', '.profile-image-input').on('change', 'profile-image-in
     };
 
     reader.readAsDataURL(file);
+});
+
+// 나의 키와 몸무게
+$(document).on('click', '.body-stats-container .select-item', function () {
+    $('.body-stats-container .select-item').removeClass('valid');
+    $(this).addClass('valid');
 });
