d05d2f4e55aa45b3c0b827107458d77d846ce8d2
YeahyunKim <insidesy4@gmail.com>
2025-07-16 13:58:17 +0900

    [Feat] 전체 동의하기 영역 UI 작업 진행 #69

---
+++ public/user/js/terms-privacy/terms-privacy.js
@@ -0,0 +1,11 @@
+import { initHeaderNav } from '../components/header-nav.js';
+
+function renderTermsPrivacy() {
+    console.log("Test");
+}
+
+$(document).ready(function () {
+    renderTermsPrivacy();
+    initHeaderNav($('#terms'));
+
+});
