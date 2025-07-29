d9750f2f640a224996dacf69893273f72ac55fa8
YeahyunKim <insidesy4@gmail.com>
2025-07-29 17:07:06 +0900

    [Fix] 마이페이지 버튼 위치 조정 / 회원가입 완료 페이지 데이터로 이름 노출 처리#90

--- public/user/js/my-page/myPage.js
+++ public/user/js/my-page/myPage.js
@@ -38,93 +38,95 @@ export function renderMyPageHTML(userBasicInfoData) {
             break;
     }
     return `
-        <div class="mypage-info-container">
-            <div class="mypage-info-goal-wrapper" data-path="info">
-                <div class="mypage-info-wrapper">
+        <div class="flex-container">
+            <div class="mypage-info-container">
+                <div class="mypage-info-goal-wrapper" data-path="info">
+                    <div class="mypage-info-wrapper">
 
-                    <div class="profile-image">
-                        <img src="/user/images/icon_human_red.png">
-                    </div>
-
-                    <div class="info-wrapper">
-                        <div class="name-wrapper">
-                            <div class="name">${userBasicInfoData.name}</div>
-                            <div class="icon">
-                                <img src="/user/images/icon_arrow_light_gray.png">
-                            </div>
+                        <div class="profile-image">
+                            <img src="/user/images/icon_human_red.png">
                         </div>
-                        <div class="message">오늘도 힘내요!</div>
+
+                        <div class="info-wrapper">
+                            <div class="name-wrapper">
+                                <div class="name">${userBasicInfoData.name}</div>
+                                <div class="icon">
+                                    <img src="/user/images/icon_arrow_light_gray.png">
+                                </div>
+                            </div>
+                            <div class="message">오늘도 힘내요!</div>
+                        </div>
                     </div>
+
+                    <div class="mypage-goal-wrapper">
+                        <div class="goal-type">
+                            <div class="type-title">${goalText}</div>
+                            <div class="sub">나의 목표</div>
+                        </div>
+
+                        <div class="divider"></div>
+
+                        <div class="goal-kcal">
+                            <div class="kcal-amount">${Math.floor(userBasicInfoData.targetKcal)}kcal</div>
+                            <div class="sub">목표 kcal</div>
+                        </div>
+
+                        <div class="divider"></div>
+
+                        <div class="goal-streak">
+                            <div class="streak-day">${userBasicInfoData.streakDay}일째</div>
+                            <div class="sub">연속 기록</div>
+                        </div>
+                    </div>
+            
                 </div>
-
-                <div class="mypage-goal-wrapper">
-                    <div class="goal-type">
-                        <div class="type-title">${goalText}</div>
-                        <div class="sub">나의 목표</div>
-                    </div>
-
-                    <div class="divider"></div>
-
-                    <div class="goal-kcal">
-                        <div class="kcal-amount">${Math.floor(userBasicInfoData.targetKcal)}kcal</div>
-                        <div class="sub">목표 kcal</div>
-                    </div>
-
-                    <div class="divider"></div>
-
-                    <div class="goal-streak">
-                        <div class="streak-day">${userBasicInfoData.streakDay}일째</div>
-                        <div class="sub">연속 기록</div>
-                    </div>
-                </div>
-        
             </div>
-        </div>
 
-        <!-- 추후 작업
-        <div class="mypage-alarm-container option-container">
-            <div class="title">알람 설정</div>
-            <div class="set-time item-option" data-path="set-time">
-                <div class="option-title">식사 시간 설정</div>
-                <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
-            </div>
-            <div class="set-alarm item-option">
-                <div class="option-title">알람 권한</div>
-                <div class="alarm-toggle"></div>
-            </div>
-        </div>
-        --!>
-
-        <div class="mypage-customer-service-container option-container">
-            <div class="title">고객 센터</div>
-            <div class="notice item-option" data-path="notice">
-                <div class="option-title">공지사항</div>
-                <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
-            </div>
             <!-- 추후 작업
-            <div class="question item-option" data-path="question">
-                <div class="option-title">문의</div>
-                <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
+            <div class="mypage-alarm-container option-container">
+                <div class="title">알람 설정</div>
+                <div class="set-time item-option" data-path="set-time">
+                    <div class="option-title">식사 시간 설정</div>
+                    <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
+                </div>
+                <div class="set-alarm item-option">
+                    <div class="option-title">알람 권한</div>
+                    <div class="alarm-toggle"></div>
+                </div>
             </div>
-             --!>
-        </div>
+            --!>
 
-        <div class="mypage-TermsAndPolicies-container option-container">
-            <div class="title">약관 및 정책</div>
-            <div class="terms-of-use item-option" data-path="terms">
-                <div class="option-title">약관 및 이용동의</div>
-                <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
+            <div class="mypage-customer-service-container option-container">
+                <div class="title">고객 센터</div>
+                <div class="notice item-option" data-path="notice">
+                    <div class="option-title">공지사항</div>
+                    <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
+                </div>
+                <!-- 추후 작업
+                <div class="question item-option" data-path="question">
+                    <div class="option-title">문의</div>
+                    <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
+                </div>
+                --!>
             </div>
-            <div class="privacy-policy item-option" data-path="privacy">
-                <div class="option-title">개인정보 보호정책</div>
-                <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
-            </div>
-        </div>
 
-        <div class="mypage-app-version-container option-container">
-            <div class="title">앱 버전</div>
-            <div class="terms-of-use item-option">
-                <div class="option-title">1.2v</div>
+            <div class="mypage-TermsAndPolicies-container option-container">
+                <div class="title">약관 및 정책</div>
+                <div class="terms-of-use item-option" data-path="terms">
+                    <div class="option-title">약관 및 이용동의</div>
+                    <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
+                </div>
+                <div class="privacy-policy item-option" data-path="privacy">
+                    <div class="option-title">개인정보 보호정책</div>
+                    <div class="icon"><img src="/user/images/icon_arrow_light_gray.png"></div>
+                </div>
+            </div>
+
+            <div class="mypage-app-version-container option-container">
+                <div class="title">앱 버전</div>
+                <div class="terms-of-use item-option">
+                    <div class="option-title">1.2v</div>
+                </div>
             </div>
         </div>
 
